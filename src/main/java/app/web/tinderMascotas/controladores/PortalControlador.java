4627caf47a016c036704e496ef7829f5688e5176
Aleidy Alfonzo <aalfonzo.mtx@gmail.com>
2021-11-03 21:13:15 -0300

    Validar formulario

--- src/main/java/app/web/tinderMascotas/controladores/PortalControlador.java
+++ src/main/java/app/web/tinderMascotas/controladores/PortalControlador.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,20 +38,30 @@ public class PortalControlador {
     }
 
     @PostMapping("/registrar")
-    public String registrar(@RequestParam String nombre, @RequestParam String apellido, @RequestParam String mail, @RequestParam String clave1, @RequestParam String clave2) {
+    public String registrar(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String mail, @RequestParam String clave1, @RequestParam String clave2) {
         try {
             usuarioServicio.registrarUsuario(null, nombre, apellido, mail, clave1);
-            System.out.println("Nombre:" + nombre);
-            System.out.println("Apellido:" + apellido);
-            System.out.println("Mail:" + mail);
-            System.out.println("Clave 1:" + clave1);
-            System.out.println("Clave 2:" + clave2);
+//            System.out.println("Nombre:" + nombre);
+//            System.out.println("Apellido:" + apellido);
+//            System.out.println("Mail:" + mail);
+//            System.out.println("Clave 1:" + clave1);
+//            System.out.println("Clave 2:" + clave2);
 
         } catch (ErrorServicio ex) {
-            Logger.getLogger(PortalControlador.class.getName()).log(Level.SEVERE, null, ex);
+            modelo.put("error", ex.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("apellido", apellido);
+            modelo.put("mail", mail);
+            modelo.put("clave1", clave1);
+            modelo.put("clave2", clave2);
+            
             return "registro.html";
         }
-        return "index.html";
+        
+        modelo.put("titulo", "Bienvenido a Tinder de Mascotas");
+        modelo.put("descripcion", "Tu usuario fue registrado de manera satisfactoria");
+        
+        return "exito.html";
     }
 
 }
